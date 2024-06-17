8576d8c3803f8465db3382410f5a5714af918c02
Vansh16aug <kumarvansh16aug@gmail.com>
2024-06-17 13:20:29 +0530

    updates

---
+++ travel-with-me/src/Stats.js
@@ -0,0 +1,24 @@
+export function Stats({ items }) {
+  const item = items.length;
+  const packed = items.filter((item) => item.packed).length;
+  const itemPercent = Math.round((packed / item) * 100) || 0;
+
+  if (item === 0) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  return (
+    <footer className="stats">
+      <em>
+        {itemPercent === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 You have ${item} items on your list, and you already packed ${packed} (
+          ${itemPercent}%) 🎒`}
+      </em>
+    </footer>
+  );
+}
