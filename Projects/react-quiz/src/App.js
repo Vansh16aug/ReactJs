6cf0e4404589151d9bec6a2cec7bcb80cda475ab
Vansh16aug <kumarvansh16aug@gmail.com>
2024-08-23 12:38:53 +0530

    update: Question fetching

--- Projects/react-quiz/src/App.js
+++ Projects/react-quiz/src/App.js
@@ -1,9 +1,67 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./common/Header";
+import Main from "./components/Main";
+import Loader from "./common/Loader";
+import Error from "./common/Error";
+import StartScreen from "./components/StartScreen";
+import Questions from "./components/Questions";
+
+const initialState = {
+  questions: [],
+  // loading , error , ready , active , finished
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Invalid action type");
+  }
+}
 
 export default function App() {
+  const [{ status, questions, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8001/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions question={questions[index]} />}
+      </Main>
     </div>
   );
 }
